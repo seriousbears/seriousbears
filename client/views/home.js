b4e5e6628815c35be21d3847990c1b060f24da34
Tyler Straub <tylerstraub@gmail.com>
2016-03-08 06:27:08 +0000

    fantastic night of progression.. dynamic scrolling viewports working.. huge improvements to layout scale.. we in dis

--- client/views/home.js
+++ client/views/home.js
@@ -1,5 +1,10 @@
 Template.home.rendered = function () {
   $(document).ready(function(){
     $('ul.tabs').tabs();
+    // good lord.. it finally works (this line will )
+    $('.primary-viewport, .outer-container').height($(window).height() - 120);
+    $( window ).resize(function() {
+      $('.primary-viewport, .outer-container').height($(window).height() - 120);
+    });
   });
 };
