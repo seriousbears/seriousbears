229b4f92dab34a9370318a0e0e34cd07803b19a7
Tyler Straub <tylerstraub@gmail.com>
2016-03-09 06:27:49 +0000

    got the intial audio play button working.. based around stateful listeners instead of click events. booyakasha..

--- client/views/home.js
+++ client/views/home.js
@@ -13,14 +13,33 @@ Template.home.events({
   'click #btn_bearstream': function(){
     console.log("You clicked the bearstream button.");
 
-    var audioElement = document.getElementById("bearstream");
-    if (audioElement.paused) {
-      audioElement.play();
+    var audioElement = $('#bearstream');
+    if (audioElement[0].paused) {
+      audioElement[0].play();
     } else {
-      audioElement.pause();
-      audioElement.src = "http://radio.seriousbears.net:8000/seriousbears.mp3";
-      audioElement.currentTime = 0;
+      audioElement[0].pause();
+      audioElement[0].src = "http://radio.seriousbears.net:8000/seriousbears.mp3";
+      audioElement[0].currentTime = 0;
     }
 
+  },
+
+  'suspend #bearstream': function(){
+    $( "#btn_icon" ).removeClass();
+    $( "#btn_icon" ).addClass("fa fa-volume-up");
+  },
+  'play #bearstream': function(){
+    console.log("The play event fired..");
+    $( "#btn_icon" ).removeClass();
+    $( "#btn_icon" ).addClass("fa fa-refresh fa-spin");
+  },
+  'pause #bearstream': function(){
+    console.log("The pause event fired..");
+  },
+  'canplay #bearstream': function(){
+    console.log("The canplay event fired..");
+    $( "#btn_icon" ).removeClass();
+    $( "#btn_icon" ).addClass("fa fa-pause");
   }
+
 });
