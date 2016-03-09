6544299330ed032b03fd12ccdb73ca48902d01a6
Tyler Straub <tylerstraub@gmail.com>
2016-03-09 01:21:42 +0000

    setup the audio button, sorta. needs a lot of work

--- client/views/home.js
+++ client/views/home.js
@@ -1,10 +1,26 @@
 Template.home.rendered = function () {
   $(document).ready(function(){
     $('ul.tabs').tabs();
-    // good lord.. it finally works (this line will )
+    // good lord.. it finally works
     $('.primary-viewport, .outer-container').height($(window).height() - 120);
     $( window ).resize(function() {
       $('.primary-viewport, .outer-container').height($(window).height() - 120);
     });
   });
 };
+
+Template.home.events({
+  'click #btn_bearstream': function(){
+    console.log("You clicked the bearstream button.");
+
+    var audioElement = document.getElementById("bearstream");
+    if (audioElement.paused) {
+      audioElement.play();
+    } else {
+      audioElement.pause();
+      audioElement.src = "http://radio.seriousbears.net:8000/seriousbears.mp3";
+      audioElement.currentTime = 0;
+    }
+
+  }
+});
