fb67526c8b72fe10c01614dc2c61a5ec4818fbc4
Tyler Straub <tylerstraub@gmail.com>
2016-03-12 20:57:23 +0000

    completed a first working draft of IRC boucner relay between twitch chat and quakenet. BOOYAKASHA #bearweekend2016

--- server/nodeirc.js
+++ server/nodeirc.js
@@ -1,4 +1,6 @@
-IRC = {};
+// define some globals for use throughout..
+QUAKEIRC = {};
+TWITCHIRC = {};
 
 Meteor.startup(function () {
 
@@ -7,34 +9,84 @@ Meteor.startup(function () {
     privatekey: '6LeuahoTAAAAAKHnENaDOI6KecpzLMmxsycVQdsc'
   });
 
-  _addListener = function(callback) {
-    IRC = new irc.Client('irc.quakenet.org', 'WilliamtonBear', {
+  _addQuakeNetListener = function(callback) {
+    QUAKEIRC = new irc.Client('irc.quakenet.org', 'williamBot', {
       channels: ['#seriousbears'],
     });
 
-    IRC.addListener('message', function (from, to, message) {
+    QUAKEIRC.addListener('message', function (from, to, message) {
       console.log(from + ' => ' + to + ': ' + message);
       callback(from, to, message);
     });
   };
 
-  var boundFunction = Meteor.bindEnvironment(function(from, to, message) {
+  _addTwitchListener = function(callback) {
+    TWITCHIRC = new irc.Client('irc.twitch.tv', 'seriousbears', {
+      port: 6667,
+      localAddress: null,
+      password: "oauth:i8bbl0i7wojnaeqswbinhb2yythlz3",
+      debug: true,
+      showErrors: true,
+      autoRejoin: true,
+      autoConnect: true,
+      nick: 'seriousbears',
+      userName: 'seriousbears',
+      realName: 'SeriousBears IRC Boucner',
+      channels: ['#seriousbears'],
+      secure: false,
+      selfSigned: false,
+      certExpired: false,
+      floodProtection: false,
+      floodProtectionDelay: 1000,
+      sasl: true,
+      stripColors: false,
+      channelPrefixes: "&#",
+      messageSplit: 512,
+      encoding: ''
+    });
+
+    TWITCHIRC.addListener('message', function (from, to, message) {
+      console.log(from + ' => ' + to + ': ' + message);
+      callback(from, to, message);
+    });
+  };
+
+  var boundQuakeInsert = Meteor.bindEnvironment(function(from, to, message) {
     BearChat.insert({
       createdAt: new Date(),
       from: from,
       to: to,
-      message: message
+      message: message,
     });
+    // additionally.. execute IRC bounce in same scope..
+    bounceTwitch(from, to, message);
   });
 
-  _addListener(boundFunction);
+  var boundTwitchInsert = Meteor.bindEnvironment(function(from, to, message) {
+    BearChat.insert({
+      createdAt: new Date(),
+      from: from,
+      to: to,
+      message: from + " (TTV) => " + message,
+    });
+    // additionally.. execute IRC bounce in same scope..
+    bounceQuakeNet(from, to, message);
+  });
 
+  function bounceQuakeNet(from, to, message) {
+    QUAKEIRC.say('#seriousbears', from + " (TTV) => " + message);
+    return;
+  }
+
+  function bounceTwitch(from, to, message) {
+    TWITCHIRC.say('#seriousbears', from + " (QUAKENET) => " + message);
+    return;
+  }
+
+  _addQuakeNetListener(boundQuakeInsert);
+  _addTwitchListener(boundTwitchInsert);
 
   Meteor.methods({
-    youClickedMe: function () {
-      IRC.say('#seriousbears', "WHY WOULD YOU PRESS THE BUTTON?!!??!");
-      return;
-    }, 
 
     formSubmissionMethod: function(formData, captchaData) {
 
@@ -53,7 +105,7 @@ Meteor.startup(function () {
       console.log(formData.name);
       console.log(formData.category);
       console.log(formData.message);
-      IRC.say('#seriousbears', "incoming " + formData.category + " from " + formData.name + ": " +  formData.message);
+      QUAKEIRC.say('#seriousbears', "incoming " + formData.category + " from " + formData.name + ": " + formData.message);
       return true;
     }
   });
